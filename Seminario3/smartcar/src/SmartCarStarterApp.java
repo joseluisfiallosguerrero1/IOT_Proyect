022f110a6c2a7b9636eddbeb6790553e083cdc74
Gary Alarcon <galarconmadrid@gmail.com>
2024-05-30 16:32:12 +0200

    Ejercicio 5.3 - Agregar limite de velocidad. Se creo la clase SpeedLimitSign

--- Seminario3/smartcar/src/SmartCarStarterApp.java
+++ Seminario3/smartcar/src/SmartCarStarterApp.java
@@ -1,5 +1,8 @@
+import java.util.UUID;
+
 import componentes.RoadPlace;
 import componentes.SmartCar;
+import componentes.SpeedLimitSign;
 
 public class SmartCarStarterApp {
     public static void main(String[] args) throws Exception {
@@ -38,5 +41,21 @@ public class SmartCarStarterApp {
 
 		//sc1.getIntoRoad(...);  // indicamos que el SmartCar está en tal segmento
 		//sc1.notifyIncident(...);
+
+
+		//Ejercicio 5.3
+		String speedlimitID = "SpeedLimitSign1" +UUID.randomUUID().toString();
+
+		// Indicate that the SmartCar is on road "R5s1" at km 50
+		int currentSpeed = 50;
+		sc1.getIntoRoad("R5s1", currentSpeed);  // indicamos que el SmartCar está en tal segmento
+		sc1.notifyIncident("INCIDENT");
+
+		// Create and connect a SpeedLimitSign
+		SpeedLimitSign speedLimitSign = new SpeedLimitSign(speedlimitID, brokerURL);
+		speedLimitSign.connect();
+		
+		// Set a new speed limit on a specific road segment
+		speedLimitSign.reportSpeedLimit("R5s1", currentSpeed, 40); 
     }
 }

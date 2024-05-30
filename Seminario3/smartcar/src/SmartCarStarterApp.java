1e0c8888597f50a00dbb2ed8f9fa937af637d016
Juan <camilojojoa@gmail.com>
2024-05-30 13:45:53 +0200

    Modificación al ejecutable SmartCarStarterApp

--- Seminario3/smartcar/src/SmartCarStarterApp.java
+++ Seminario3/smartcar/src/SmartCarStarterApp.java
@@ -1,3 +1,4 @@
+import componentes.RoadPlace;
 import componentes.SmartCar;
 
 public class SmartCarStarterApp {
@@ -13,13 +14,29 @@ public class SmartCarStarterApp {
 		String brokerURL = args[1];
 
         SmartCar sc1 = new SmartCar(smartCarID, brokerURL);
-		
+		sc1.setCurrentRoadPlace(new RoadPlace("R1s1", 0));
+
+        SmartCar sc2 = new SmartCar(smartCarID + "2", brokerURL);
+		sc2.setCurrentRoadPlace(new RoadPlace("R1s1", 0));
+
+        SmartCar sc3 = new SmartCar(smartCarID + "2", brokerURL);
+		sc3.setCurrentRoadPlace(new RoadPlace("R1s1", 0));
+
+        SmartCar sc4 = new SmartCar(smartCarID + "4", brokerURL);
+		sc4.setCurrentRoadPlace(new RoadPlace("R1s1", 0));
+
+        SmartCar sc5 = new SmartCar(smartCarID + "5", brokerURL);
+		sc5.setCurrentRoadPlace(new RoadPlace("R1s1", 0));
+
+        SmartCar sc6 = new SmartCar(smartCarID + "6", brokerURL);
+		sc6.setCurrentRoadPlace(new RoadPlace("R1s1", 0));
+
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 		}
 
-		sc1.getIntoRoad(...);  // indicamos que el SmartCar está en tal segmento
-		sc1.notifyIncident(...);
+		//sc1.getIntoRoad(...);  // indicamos que el SmartCar está en tal segmento
+		//sc1.notifyIncident(...);
     }
 }
